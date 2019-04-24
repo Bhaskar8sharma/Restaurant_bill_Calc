c670a018ca19adfba8d0207921d3fe199dfe4bca
Bhaskar Sharma <bhaskarsharma0789@gmail.com>
2019-04-23 23:31:37 -0400

    Added Restaurant bill calculator C#

---
+++ Restaurant_bill/Restaurant_bill/MainCourse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_bill
+{
+    public class MainCourse
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int quantity { get; set; }
+        public double total
+        {
+            get
+            {
+                return Price * quantity;
+            }
+            set
+            {
+                total = value;
+            }
+        }
+        public double tax
+        {
+            get
+            {
+                return total * 0.13;
+            }
+            set
+            {
+                tax = value;
+            }
+        }
+    }
+}
